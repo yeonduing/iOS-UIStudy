8e74a240e320bd645095f48a216ac85dfd739a2d
itzel.du <itzel.du@kakaoenterprise.com>
2021-07-13 02:05:14 +0900

    프로젝트 파일 생성 및 gitignore 설정

---
+++ iOS-UIStudy/iOS-UIStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-UIStudy
+//
+//  Created by itzel.du on 2021/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
